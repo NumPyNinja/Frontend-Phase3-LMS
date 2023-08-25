f074da61a14ee43c38ff66238c23bb087e8d9213
NumPyNinja <68971671+NumPyNinja@users.noreply.github.com>
2023-08-24 19:22:09 -0500

    Update sever.js

--- sever.js
+++ sever.js
@@ -9,7 +9,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 const HOST = "localhost";
-const API_SERVICE_URL = "https://lms-phase2.herokuapp.com/lms/";
+const API_SERVICE_URL = "https://lms-backend-service.herokuapp.com/lms/";
 
 // Proxy endpoints
 app.use('/api', createProxyMiddleware({
